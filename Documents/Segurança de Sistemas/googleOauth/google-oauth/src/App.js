10c46576f9582e7ab088c116c3f83f9db31a3a8d
Amanda <amandaahk15@gmail.com>
2021-10-13 15:03:37 -0300

    login and logout

--- Documents/Segurança de Sistemas/googleOauth/google-oauth/src/App.js
+++ Documents/Segurança de Sistemas/googleOauth/google-oauth/src/App.js
@@ -1,27 +1,49 @@
-import React, { Component } from 'react'
-import GoogleLogin from 'react-google-login'
-export class App extends Component {
+import React, { Component } from "react";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
-  responseGoogle=(response)=>{
-    console.log(response);
-    console.log(response.profileObj);
-    
-    
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
   }
   render() {
     return (
-      <div>
-        <GoogleLogin
-        clientId="276655996971-tqcgsfqvun9d061loj9t47demuhbqb9l.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={this.responseGoogle}
-        onFailure={this.responseGoogle}
-        cookiePolicy={'single_host_origin'}
-        
-        />
+      <div align="center">
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">SEGURANÇA DE SISTEMAS - OAUTH</h1>
+        </header>
+
+        {!this.state.isLoggedIn ? (
+          <GoogleLogin
+            className="GoogButton"
+            clientId="230901459538-ig0m9156174k09r5rohj4s6qv6qv5rjd.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={response => {
+              console.log(response)
+              this.setState(() => {
+                return { isLoggedIn: true };
+              });
+            }}
+            onFailure={response => {
+              this.setState(() => {
+                return { isLoggedIn: false };
+              });
+            }}
+          />
+        ) : (
+          <GoogleLogout
+            buttonText="Logout"
+            onLogoutSuccess={response => {
+              this.setState(() => {
+                return { isLoggedIn: false };
+              });
+            }}
+          />
+        )}
       </div>
-    )
+      </div>
+    );
   }
 }
-
-export default App
+export default App;
